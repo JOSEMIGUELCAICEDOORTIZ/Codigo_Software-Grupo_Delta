0df5ae517291444749c28ff13f68591d738cf3aa
JOSEMIGUELCAICEDOORTIZ <130945154+JOSEMIGUELCAICEDOORTIZ@users.noreply.github.com>
2023-05-14 22:28:45 -0500

    Add files via upload

---
+++ sketch_may14c.ino
@@ -0,0 +1,20 @@
+//Prueba de la valvula mariposa
+#include<Servo.h>
+Servo myServo;
+int const PotPin=A0;
+int PotVal;
+int angle;
+void setup(){
+myServo.attach(9);
+Serial.begin(9600);
+}
+void loop(){
+PotVal=analogRead(PotPin);
+Serial.print("PotVal:");
+Serial.print(PotVal);
+angle=map(PotVal,0,1023,0,179);
+Serial.print ("angle= ");
+Serial.println (angle);
+myServo.write(angle);
+delay (15);
+}
